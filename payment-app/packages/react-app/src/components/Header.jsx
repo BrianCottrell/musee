76291800ef10110fdf8ca85530cb5f39899820b5
BrianCottrell <Brian.Cottrell.mobile@gmail.com>
2022-06-06 16:41:59 -0700

    Updated NFTs

--- payment-app/packages/react-app/src/components/Header.jsx
+++ payment-app/packages/react-app/src/components/Header.jsx
@@ -9,11 +9,8 @@ export default function Header({ link, title, subTitle, ...props }) {
   return (
     <div style={{ display: "flex", justifyContent: "space-between", padding: "1.2rem" }}>
       <div style={{ display: "flex",  flexDirection: "column", flex: 1, alignItems: "center", marginLeft: 350 }}>
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          <img
-            src={'https://res.cloudinary.com/ideation/image/upload/w_128,h_128,q_auto,f_auto,dpr_auto/zdvlrruwodkfednpomkj'}
-            style={{ borderRadius: 64 }}
-          />
+        <a href={'https://xd.adobe.com/view/816b660f-44d0-496e-b516-e63fbe322a2e-d438/screen/04017962-f5b9-451b-90f5-55f84b9e0f3e?fullscreen&hints=off'} target="_blank" rel="noopener noreferrer">
+          <img src={'https://res.cloudinary.com/ideation/image/upload/w_128,h_128,q_auto,f_auto,dpr_auto/irvvs0ozhnewcecly5ic'} />
         </a>
         <Text type="secondary" style={{ textAlign: "center" }}>{subTitle}</Text>
       </div>
@@ -25,5 +22,5 @@ export default function Header({ link, title, subTitle, ...props }) {
 Header.defaultProps = {
   link: "https://platform.zencon.bemyapp.com/#/projects/629cd083494b7c002d1f5e21",
   title: "Musee",
-  subTitle: "Maximazing the work efficency and ganancies to every musician in the world",
+  subTitle: "Maximizing the work efficency and ganancies to every musician in the world",
 };
